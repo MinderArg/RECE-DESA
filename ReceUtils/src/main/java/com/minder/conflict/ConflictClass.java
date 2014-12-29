8690f1e902e1d6481bae8f1e8879e60d4ec7f91b
DanielF <daniel.facciabene@minder.cc>
2014-12-29 16:44:17 -0300

    Step#6

--- ReceUtils/src/main/java/com/minder/conflict/ConflictClass.java
+++ ReceUtils/src/main/java/com/minder/conflict/ConflictClass.java
@@ -4,7 +4,12 @@ public class ConflictClass implements Conflictable {
 
 	@Override
 	public void doConflict() {
-		System.out.println("I am a conflict!");
+		System.out.println("I am still a conflict!");
+	}
+
+	@Override
+	public void doTheDanny() {
+		System.out.println("Doing the danny!");
 	}
 	
 }
