e0d94466f43e7ec8b92c343415cb4bf9c3db3f09
DanielF <daniel.facciabene@minder.cc>
2015-01-19 15:14:39 -0300

    Clases completas con Period y AuditData.

---
+++ ReceUtils/src/main/java/com/minder/rece/domain/AuditData.java
@@ -0,0 +1,46 @@
+package com.minder.rece.domain;
+
+import java.util.Date;
+
+import javax.persistence.*;
+
+@Embeddable
+public class AuditData {
+
+	@Column(name = "audit_creation_date")
+	private Date creationDate;
+
+	public Date getCreationDate() {
+		return creationDate;
+	}
+
+	public void setCreationDate(Date creationDate) {
+		this.creationDate = creationDate;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((creationDate == null) ? 0 : creationDate.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		AuditData other = (AuditData) obj;
+		if (creationDate == null) {
+			if (other.creationDate != null)
+				return false;
+		} else if (!creationDate.equals(other.creationDate))
+			return false;
+		return true;
+	}
+
+}
