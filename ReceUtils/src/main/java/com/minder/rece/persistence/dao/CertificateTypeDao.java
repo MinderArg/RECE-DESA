7fe91bea27b0e0a42f263851130cf8892041b707
Lucas Ponce de León <lucas.ponce@minder.cc>
2015-01-20 17:14:00 -0300

    Comienzo de implementaciones de DAO: CertificateType

---
+++ ReceUtils/src/main/java/com/minder/rece/persistence/dao/CertificateTypeDao.java
@@ -0,0 +1,8 @@
+package com.minder.rece.persistence.dao;
+
+import com.minder.rece.domain.CertificateType;
+import com.minder.rece.persistence.BaseDao;
+
+public interface CertificateTypeDao extends BaseDao<CertificateType, Integer> {
+
+}
